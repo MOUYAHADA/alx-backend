f49cfbb1fe516fddd31ed6f18b36fbb95414b47e
Mohamed Mouyahada <mouyahada@gmail.com>
2024-10-28 21:58:02 +0100

    helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """page index helper function"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
